a3c00691a964d8ca17aa2304a8c80fe718903ded
rohan-das <hello.rd24@gmail.com>
2026-05-28 12:53:00 +0530

    Setting up the database Vector Store in Postgres

---
+++ create-table.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE TABLE documents (
+    id text PRIMARY KEY,
+    content text NOT NULL,
+    metadata jsonb NOT NULL default '{}'::jsonb,
+    embedding vector(768) NOT NULL,
+    created_at timestamp with time zone NOT NULL default now()
+);
+
+CREATE INDEX documents_embedding_idx ON documents USING hnsw (embedding vector_cosine_ops);
+
